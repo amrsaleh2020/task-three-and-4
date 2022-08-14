5e77eb90241322382cc032608e2a5a75512ba363
amrsaleh2020 <amrsaleh018@gmail.com>
2022-08-14 18:13:06 +0200

    first commit

---
+++ MYdslib.py
@@ -0,0 +1,13 @@
+x=23
+def max(num1,num2):
+    if num1>num2:
+        return num1
+    else:
+        return num2
+
+def printName():
+    print("amr ahmed saleh")
+def returnvalue(x):
+    return x+10
+
+myDic= {"name": "amr" , "age": 22 }
